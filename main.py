db31407c575f5e31cae1c41b89edabd068e6d166
Micky Conca <52582017+Mickyconca@users.noreply.github.com>
2023-10-01 15:40:17 -0300

    Update main

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ class PomodoroApp:
         self.root = root
         self.root.geometry("800x600")
         self.root.title("Pomodoro Timer")
-        self.root.resizable(False, False)
 
         self.pomodoro_time = 25 * 60
         self.break_time = 5 * 60
@@ -18,8 +17,7 @@ class PomodoroApp:
         self.timer_label = tk.Label(self.root, font=('Helvetica', 48), bg='green')
         self.timer_label.pack(pady=100)
 
-        self.start_button = tk.Button(self.root, text="Iniciar Pomodoro", command=self.start_pomodoro)
-        self.start_button.pack()
+        self.start_button = tk.Button(self.root, text="Iniciar", command=self.start_pomodoro)
 
         self.pause_button = tk.Button(self.root, text="Pausar", command=self.pause_timer)
         self.pause_button.pack(pady=20)
@@ -39,10 +37,10 @@ class PomodoroApp:
     def toggle_automatic_mode(self):
         self.automatic_mode = not self.automatic_mode
         if self.automatic_mode:
-            self.automatic_button.config(text="Detener Pomodoro Automático")
+            self.automatic_button.config(text="Detener Pomodoro")
             self.start_pomodoro()
         else:
-            self.automatic_button.config(text="Iniciar Pomodoro Automático")
+            self.automatic_button.config(text="Iniciar Pomodoro")
             self.stop_timer()
 
     def pause_timer(self):
@@ -60,9 +58,10 @@ class PomodoroApp:
         self.start_button.config(text="Detener", command=self.stop_timer)
         self.update_timer()
 
-    def start_short_break(self):
-        self.mode = "short_break"
+    def start_break(self):
+        self.mode = "break"
         self.remaining_time = self.break_time
+        self.set_background_color("red")
         self.timer_label.config(bg="red")
         self.start_button.config(text="Detener", command=self.stop_timer)
         self.update_timer()
@@ -86,19 +85,21 @@ class PomodoroApp:
         else:
             if self.automatic_mode:
                 self.play_sound()
-
             if self.mode == "pomodoro":
-                self.start_button.config(text="Iniciar Recreo Corto", command=self.start_short_break)
+                self.start_button.config(text="Iniciar Recreo", command=self.start_break)
                 if self.automatic_mode:
-                    self.start_short_break()
-            elif self.mode == "short_break":
+                    self.start_break()
+            elif self.mode == "break":
                 self.start_button.config(text="Iniciar Pomodoro", command=self.start_pomodoro)
                 if self.automatic_mode:
                     self.start_pomodoro()
 
+    def set_background_color(self, color):
+        self.root.configure(bg=color)
+
     def configure_times(self):
         self.pomodoro_time = simpledialog.askinteger("Configurar", "Tiempo del Pomodoro (en minutos)", minvalue=1) * 60
-        self.break_time = simpledialog.askinteger("Configurar", "Tiempo de recreo corto (en minutos)",
+        self.break_time = simpledialog.askinteger("Configurar", "Tiempo de recreo (en minutos)",
                                                         minvalue=1) * 60
 
 
